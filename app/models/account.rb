e9ff9f1db73615af3f2bf715cd1fcad41eb3ba7d
Damian Kosowski <damian.kosowski@netguru.pl>
2018-12-14 13:29:13 +0100

    Add async indexing

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,6 +4,9 @@ class Account < ActiveRecord::Base
   index_name "bank"
   document_type "account"
 
+  after_save    { IndexWorker.perform_async(:index, self.class.name, self.id) }
+  after_destroy { IndexWorker.perform_async(:delete, self.class.name, self.id) }
+
   mapping do
     indexes :address, type: 'text'
   end
